997a434eb168401390cc37c04f986a53be2ed11e
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2024-03-08 12:08:31 +0100

    GitHub Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.dokka)
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+}
+
+tasks.dokkaHtmlMultiModule.configure {
+    outputDirectory.set(rootDir.resolve("docs"))
 }
